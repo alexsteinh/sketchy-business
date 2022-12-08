209d2887ffc5814abfd1e858950844b6a049584a
Alexander Steinhauer <alexander.steinhauer@check24.de>
2022-12-08 11:14:40 +0100

    Initial Commit

---
+++ SketchyBusiness/SketchyBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchyBusinessApp.swift
+//  SketchyBusiness
+//
+//  Created by Alexander Steinhauer on 08.12.22.
+//
+
+import SwiftUI
+
+@main
+struct SketchyBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
